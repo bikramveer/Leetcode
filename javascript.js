42a287073fd0af22045144faf94952e3713a193c
bikramveer <bikramveergill@gmail.com>
2025-09-08 16:35:12 -0400

    completed question 66: Plus One, in Javascript

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+///////////////////////// 1: TWO SUM /////////////////////////
 // var twoSum = function(nums, target) {
 //     for(let i = 0; i < nums.length; i++) {
 //         for(let j = 0; j < nums.length; j++) {
@@ -26,6 +27,8 @@
 // let target = 6
 // console.log(twoSum2(nums, target))
 
+
+///////////////////////// 58: LENGTH OF LAST WORD /////////////////////////
 // var lengthOfLastWord = function(s) {
 //     let count = 0;
 //     for(let i = s.length-1; i >= 0; i--) {
@@ -44,3 +47,28 @@
 
 // const s = 'a';
 // console.log(lengthOfLastWord(s));
+
+
+///////////////////////// 66: PLUS ONE /////////////////////////
+var plusOne = function(digits) {
+    let curr = digits.length - 1;
+    while(curr >= 0) {
+        digits[curr]++;
+        if(digits[curr] == 10) {
+            digits[curr] = 0;
+            if(curr == 0) {
+                digits = [1].concat(digits);
+                console.log('last digit was a 9: ' + digits);
+                return;
+            }
+            console.log('end digit was a 9: ' + digits);
+            curr--;
+        } else {
+            console.log('last digit was not a 9: ' + digits);
+            return;
+        }
+    }
+}
+
+let digits = [2,3,7,9,9];
+plusOne(digits);
